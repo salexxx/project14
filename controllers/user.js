5a8919ad8ab0d159e6217a34c9d4a0165d1b3328
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-07-22 16:59:21 +0700

    установил линтер исправил ошибки линтера

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
-const user = require('../models/user');
-const bcrypt = require('bcryptjs'); 
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const user = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   user.find({})
@@ -10,13 +10,13 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.createUser = (req, res) => {
   bcrypt.hash(req.body.password, 10)
-  .then(hash =>  user.create({
-    email: req.body.email,
-    password: hash,
-    name: req.body.name,
-    about: req.body.about,
-    avatar: req.body.avatar,
-  }))
+    .then((hash) => user.create({
+      email: req.body.email,
+      password: hash,
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar,
+    }))
     .then((users) => res.status(200).send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
@@ -29,16 +29,15 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.login = (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   return user.findUserByCredentials(email, password)
-  .then((user) => {
-   const token = jwt.sign({_id: user._id},{expiresIn: '7d'});
-   res.send({token});
-  })
-  .catch((err) => {
-    res
-    .status(401)
-    .send({message: err.message});
-
-  })
-}
+    .then((usr) => {
+      const token = jwt.sign({ _id: usr._id }, { expiresIn: '7d' });
+      res.send({ token });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+};
