b2673d4cc3d2d95d9183c73e6055e35903233e33
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-07-22 17:18:40 +0700

    исправил ошибки линтинга в user.js

--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     unique: true,
-    validate:{
+    validate: {
       validator: (mail) => validator.isEmail(mail),
       message: 'Введите адрес электронной почты',
-    }
+    },
   },
   password: {
     type: String,
@@ -37,20 +37,20 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-userSchema.static.findUserByCredentials = function(email, password) {
-return this.findOne({email})
-.then((user)=>{
-  if(!user){
-    return Promise.reject(new Error('неправильные почта или пароль'));
-  }
-  return bcrypt.compare(password, user.password)
-.then((matched)=>{
-  if(!matched){
-    return Promise.reject(new Error('неправильные почта иил пароль'));
-  }
-  return(user);
-})
-})
+userSchema.static.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('неправильные почта иил пароль'));
+          }
+          return (user);
+        });
+    });
 };
 
 module.exports = mongoose.model('user', userSchema);
