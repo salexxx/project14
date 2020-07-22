ea4f7eb383ad19c53cb131a8bb229b4718613bbf
Aleksei Smolyaninov <salexxx@ngs.ru>
2020-07-22 16:31:50 +0700

    создал контроллер login

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,13 @@
 const user = require('../models/user');
 const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
 
 module.exports.getUsers = (req, res) => {
   user.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
 module.exports.createUser = (req, res) => {
   bcrypt.hash(req.body.password, 10)
   .then(hash =>  user.create({
@@ -18,9 +20,25 @@ module.exports.createUser = (req, res) => {
     .then((users) => res.status(200).send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
 module.exports.getUser = (req, res) => {
   user
     .findById(req.params.id).orFail()
     .then((someuser) => res.send({ data: someuser }))
     .catch((err) => res.status(404).send({ err, message: 'Пользователь не найден' }));
 };
+
+module.exports.login = (req, res) => {
+  const {email, password} = req.body;
+  return user.findUserByCredentials(email, password)
+  .then((user) => {
+   const token = jwt.sign({_id: user._id},{expiresIn: '7d'});
+   res.send({token});
+  })
+  .catch((err) => {
+    res
+    .status(401)
+    .send({message: err.message});
+
+  })
+}
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -36,5 +37,20 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+userSchema.static.findUserByCredentials = function(email, password) {
+return this.findOne({email})
+.then((user)=>{
+  if(!user){
+    return Promise.reject(new Error('неправильные почта или пароль'));
+  }
+  return bcrypt.compare(password, user.password)
+.then((matched)=>{
+  if(!matched){
+    return Promise.reject(new Error('неправильные почта иил пароль'));
+  }
+  return(user);
+})
+})
+};
 
 module.exports = mongoose.model('user', userSchema);
